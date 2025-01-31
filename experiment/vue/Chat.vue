a142dec6111d34a33e42e24c05c32be98fb82222
Declan Simkins <declan.simkins@gmail.com>
2025-01-31 14:15:53 -0700

    add ai generated vue app

---
+++ experiment/vue/Chat.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="messages">
+      <div v-for="message in messages" :key="message.id">{{ message.text }}</div>
+    </div>
+    <input v-model="newMessage" @keyup.enter="sendMessage" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      messages: [],
+      newMessage: ''
+    }
+  },
+  methods: {
+    sendMessage() {
+      if (this.newMessage.trim()) {
+        this.messages.push({ id: Date.now(), text: this.newMessage });
+        this.newMessage = '';
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.messages {
+  max-height: 200px;
+  overflow-y: auto;
+}
+</style>
